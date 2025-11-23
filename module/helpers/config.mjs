219d6b36677470ef918ad108c574fd4a9fb3a9ab
Rafael Aguiar <terule@gmail.com>
2025-11-23 15:19:58 -0300

    ✨ (CHANGELOG.md, LICENSE.txt, README.md, src/scss, lang/en.json, module, templates): add initial files and structure for the Rising Steel system for FoundryVTT 📝 (CHANGELOG.md): document the addition of support for Foundry v10 💡 (README.md): provide usage instructions and system details for developers ♻️ (scss): create styles for the Rising Steel system, including layout and components ✅ (module): implement core functionality for actors and items, including data preparation and roll handling 🌐 (lang/en.json): add localization support for various system labels and attributes

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const RISING_STEEL = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+RISING_STEEL.abilities = {
+  str: 'RISING_STEEL.Ability.Str.long',
+  dex: 'RISING_STEEL.Ability.Dex.long',
+  con: 'RISING_STEEL.Ability.Con.long',
+  int: 'RISING_STEEL.Ability.Int.long',
+  wis: 'RISING_STEEL.Ability.Wis.long',
+  cha: 'RISING_STEEL.Ability.Cha.long',
+};
+
+RISING_STEEL.abilityAbbreviations = {
+  str: 'RISING_STEEL.Ability.Str.abbr',
+  dex: 'RISING_STEEL.Ability.Dex.abbr',
+  con: 'RISING_STEEL.Ability.Con.abbr',
+  int: 'RISING_STEEL.Ability.Int.abbr',
+  wis: 'RISING_STEEL.Ability.Wis.abbr',
+  cha: 'RISING_STEEL.Ability.Cha.abbr',
+};
